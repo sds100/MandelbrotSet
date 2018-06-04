9b9e301c9140f28eafe90734d02b0f3614c9fa58
sds2001 <seth.d.schroeder@gmail.com>
2018-06-04 18:32:49 +0100

    Resources.resx: Update version to 1.1

--- MandelbrotSet/Properties/Resources.Designer.cs
+++ MandelbrotSet/Properties/Resources.Designer.cs
@@ -61,7 +61,7 @@ namespace MandelbrotSet.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 1.0.
+        ///   Looks up a localized string similar to 1.1.
         /// </summary>
         internal static string Version {
             get {
