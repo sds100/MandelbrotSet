19963c1489bbeda11daab1afbcd03e0a9362ed41
Seth Schroeder <seth.d.schroeder@gmail.com>
2018-06-20 19:04:55 +0100

    Remove unnecessary "using" statements

--- MandelbrotSet/ImageInfo.cs
+++ MandelbrotSet/ImageInfo.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MandelbrotSet
+﻿namespace MandelbrotSet
 {
     /// <summary>
     /// Stores necessary information to recreate an image.
