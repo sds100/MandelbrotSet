51dad927c571bbc8429b0bf15d3fc0143732b224
Seth Schroeder <seth.d.schroeder@gmail.com>
2018-06-22 17:07:48 +0100

    Update version to 1.2

--- MandelbrotSet/Properties/Resources.Designer.cs
+++ MandelbrotSet/Properties/Resources.Designer.cs
@@ -61,7 +61,7 @@ namespace MandelbrotSet.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 1.1.
+        ///   Looks up a localized string similar to 1.2.
         /// </summary>
         internal static string Version {
             get {
